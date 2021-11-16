73134d901f99c95192463ecc77ed13ae6d39c99a
zhangzhiwen <zhangzhiwen@pateo.com.cn>
2021-11-16 17:25:05 +0800

    [GenericApi/main] add baseapplication,baseactivity

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 学习文档：
 1.github 基本配置：
 https://blog.csdn.net/whitesun123/article/details/79600179
+2.MVP 架构demo
+https://github.com/Othershe/NiceRead
+3.MVVM 架构demo
+https://github.com/LyndonChin/MasteringAndroidDataBinding
---
+++ app/src/main/java/com/ptshell/genericapi/ui/activity/MainActivity.java
@@ -0,0 +1,38 @@
+package com.ptshell.genericapi.ui.activity;
+
+import android.os.Bundle;
+
+import com.ptshell.genericapi.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setContentView() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void initActivity() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListen() {
+
+    }
+}
